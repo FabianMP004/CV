b9015b894cdce22e2419426ac68a5951eae45ef7
Fabian.MP.04 <fabianmiranda@ufm.edu>
2025-09-29 20:35:50 -0600

    Add initial component structure for CV sections
    Introduced new Angular components for aside, education-experience, footer, header, main-content, and tools-skills, each with their own HTML, CSS, TypeScript, and test files. This sets up the basic structure for the application's main sections.

---
+++ cv-angular-2/src/app/tools-skills/tools-skills.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ToolsSkills } from './tools-skills';
+
+describe('ToolsSkills', () => {
+  let component: ToolsSkills;
+  let fixture: ComponentFixture<ToolsSkills>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ToolsSkills]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ToolsSkills);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
