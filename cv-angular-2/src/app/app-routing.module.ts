ed1e4e0c4246740744d95aa8b117f942fa36f815
Fabian.MP.04 <fabianmiranda@ufm.edu>
2025-10-01 20:59:52 -0600

    Add initial Angular routing module
    Created app-routing.module.ts with a basic NgModule setup and an empty routes array to prepare for future route definitions.

---
+++ cv-angular-2/src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  // Las rutas se agregarán en el siguiente paso
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
