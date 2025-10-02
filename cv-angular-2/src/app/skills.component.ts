226bf1f72714245951cb67c348a6f408fefb09a3
Fabian.MP.04 <fabianmiranda@ufm.edu>
2025-10-01 21:03:21 -0600

    Add ReversePipe and update skills component
    Introduced a new standalone ReversePipe to reverse strings. Updated the skills component to use both UpperCasePipe and the new ReversePipe for displaying each skill in uppercase and reversed formats.

--- cv-angular-2/src/app/skills.component.ts
+++ cv-angular-2/src/app/skills.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { DataService } from './data.service';
+import { ReversePipe } from './reverse.pipe';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
+  imports: [ReversePipe, UpperCasePipe],
   template: `
     <h2>Habilidades</h2>
     <ul>
-      <li *ngFor="let skill of data.skills">{{ skill }}</li>
+      <li *ngFor="let skill of data.skills">
+        {{ skill | uppercase }} | {{ skill | reverse }}
+      </li>
     </ul>
   `
 })
